1b359c9425c345a1ff83d2bd2ba577f58f05513b
ryanxu <ryanxu@100.100.57.75>
2017-07-22 15:45:40 +0800

    kodo

--- src/com/qiniu/kodo/QiniuUpload.java
+++ src/com/qiniu/kodo/QiniuUpload.java
@@ -22,13 +22,13 @@ import com.qiniu.util.StringMap;
 
 
 /**
- * ��ţ���ϴ�
+ * 七牛云上传 
  * @author xuhuanchao
  *
  */
 public class QiniuUpload {
 	
-	//������Կ
+	//指定 AK, SK
 	private static final String ACCESS_KEY = "0ZT-Rd0AswhPQti5lX2Ytt1T6XkyM80eY_4w9Pm9";
 	private static final String SECRECT_KEY = "MbscrgLx_FefkUZ21SjY-GRE1oPJcvP2vvN6oXgW";
 
@@ -38,11 +38,11 @@ public class QiniuUpload {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//1. ͨ���ļ�·�������ϴ�
+		//1. 七牛简单上传
 		String filePath = "/Users/ryanxu/Downloads/imgs/stage.jpg";
 		upload(filePath, "test_stage.jpg", "test-bucket");
 		
-		//2. ͨ���ļ���ת�ֽ���������ϴ�
+		//2.
 //		File file = new File(filePath);
 //		int length = Integer.parseInt(String.valueOf(file.length()));
 //		byte[] data = new byte[length];
@@ -57,11 +57,11 @@ public class QiniuUpload {
 //		}
 		
 		
-//		//3. ���ϴ�
+//		//3. 
 //		boolean flag = uploadByForm(filePath, "test-bucket");
 //		System.out.println(flag + "");
 		
-		//4. ���Է�Ƭ�ϴ�
+		//4. 
 //		String bigFilePath = "F:\\admin\\Mysql\\navicatformysql.zip";
 //		uploadBySlice(bigFilePath, "test-bucket");
 		
@@ -97,25 +97,25 @@ public class QiniuUpload {
 	}
 	
 	/**
-	 * �ϴ��ļ��� ͨ���ֽ���������ϴ�
+	 * 上传文件， 通过字节数组的形式进行上传 
 	 * @param data
-	 * @param key �ϴ�����ţ�ռ� �洢���ļ�����
+	 * @param key 存在七牛空间Bucket 中的名称
 	 * @param bucketName
 	 */
 	public static void uploadByByteArray(byte[] data, String key, String bucketName) {
-		//
+		//1. 构建授权对象
 		Auth auth = Auth.create(ACCESS_KEY, SECRECT_KEY);
-		
-		//
+		//2. 获取上传 
 		String uploadToken = auth.uploadToken(bucketName);
-		
-		//
+		//3. 构建Configuration 对象， 指定上传空间 所在的存储区域
 		Configuration cfg = new Configuration(Zone.zone0());
-//		Zone.autoZone()
+		//4. 构建上传管理对象， 需要 Configuration 对象 作为参数
 		UploadManager uploadMgr = new UploadManager(cfg);
 		try{
+			//5. 简单上传
 			Response resp = uploadMgr.put(data, key, uploadToken);
 			if (resp.isOK()) {
+				//最后打印返回结果 response info 
 				System.out.println(resp.toString());
 				System.out.println(resp.bodyString());
 			}
@@ -127,13 +127,12 @@ public class QiniuUpload {
 	
 	
 	/**
-	 * Post��ʽ�ύ���� ͨ�����ϴ��ļ�
+	 * Post 表单上传， 通过OKHttp包
 	 * @param filePath
 	 * @return
 	 */
 	public static boolean uploadByForm(String filePath, String bucketName) {
 		boolean flag = false;
-		//1. ��ȡ��Ȩ����Auth
 		Auth auth = Auth.create(ACCESS_KEY, SECRECT_KEY);
 		OkHttpClient client = new OkHttpClient();
 		String url = "http://upload.qiniu.com/";
@@ -165,14 +164,14 @@ public class QiniuUpload {
 	
 	
 	/**
-	 * ��Ƭ�ϴ�
+	 * 上传本地文件， 使用分片上传
 	 * @param filePath
 	 * @param bucketName
 	 * @return
 	 */
 	public static boolean uploadBySlice(String filePath, String bucketName) {
 		boolean flag = false;
-		//1. ��ȡ��Ȩ����Auth
+		
 		Auth auth = Auth.create(ACCESS_KEY, SECRECT_KEY);
 		Configuration cfg = new Configuration(Zone.zone0());
 		String directory = "D:\\Downloads\\qiniu\\slice_recorder";
@@ -200,7 +199,7 @@ public class QiniuUpload {
 		Configuration cfg = new Configuration(Zone.zone0());
 		UploadManager uploadMgr = new UploadManager(cfg);
 		
-		String uploadToken = auth.uploadToken(bucketName, key);
+		String uploadToken = auth.uploadToken(bucketName, key); //区别于 简单上传最明显的地方， 多了一个 key 作为参数
 		try {
 			Response response = uploadMgr.put(filePath, key, uploadToken);
 			System.out.println(response.bodyString());
